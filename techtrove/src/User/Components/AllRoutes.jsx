15285f4814e1c6754b595ba4075c24b35568e895
Gyan Prakash <gyan2501998@gmail.com>
2023-03-31 12:52:02 +0530

    added day 2

--- techtrove/src/User/Components/AllRoutes.jsx
+++ techtrove/src/User/Components/AllRoutes.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/Home";
+import Productspage from "../Pages/Productspage";
 
 const AllRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+      <Route path="/products" element={<Productspage />} />
+
       <Route path="/cart" />
     </Routes>
   );
